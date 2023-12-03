a787c82e87cbda4c4128cd368afdb0e0582dabdb
FA_pew <you@example.comfapew07@gmail.com>
2023-12-03 22:21:31 +0100

    MobileCompatibleUI_CSScleaned

--- src/main.js
+++ src/main.js
@@ -1,48 +1,85 @@
-console.log("main.js")
-import * as THREE from 'three'
-import WebGL from 'three/addons/capabilities/WebGL.js'
+console.log("main.js");
+import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
 
-if ( WebGL.isWebGLAvailable() ) {
+let bPhone = false;
+let resizing = false;
+let lastKnownSize = { width: 0, height: 0 };
 
-	const scene = new THREE.Scene()
-
-    const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000)
-    camera.position.set(0, .5, 7)
-    camera.rotation.set(-0.23, 0, 0)
-
-    const renderer = new THREE.WebGLRenderer()
-    renderer.setClearColor("#cabfa3")
-    renderer.setSize(window.innerWidth, window.innerHeight)
-
-    document.body.appendChild(renderer.domElement)
-
-    window.addEventListener("resize", () => {
-        renderer.setSize(window.innerWidth, window.innerHeight)
-        camera.aspect = window.innerWidth / window.innerHeight
-
-        camera.updateProjectionMatrix()
-    })
-
-    const geometry = new THREE.BoxGeometry(10, .3, 10)
-    const material = new THREE.MeshStandardMaterial({color: 0x101010})
-    const mesh = new THREE.Mesh(geometry, material)
-    mesh.rotation.set(.3, 0, 0)
-    scene.add(mesh)
-
-    const light = new THREE.PointLight(0xffffff, 500, 500)
-    light.position.set(0, 10, 0)
-    scene.add(light)
-
-    var render = function() {
-        requestAnimationFrame(render)
-        mesh.rotation.y += 0.003;
-        renderer.render(scene, camera)
-    }
-    render()
-
-} else {
-
-	const warning = WebGL.getWebGLErrorMessage();
-	document.getElementById( 'container' ).appendChild( warning );
-
+if (window.innerWidth < 647) {
+    bPhone = true;
 }
+
+function init() {
+    if (WebGL.isWebGLAvailable()) {
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(60, 1, 0.1, 1000);
+        camera.position.set(0, 0.5, 9);
+        camera.rotation.set(-0.23, 0, 0);
+
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setClearColor("#cabfa3");
+        const canvasContainer = document.getElementById('map');
+        canvasContainer.appendChild(renderer.domElement);
+
+        const geometry = new THREE.BoxGeometry(10, 0.3, 10);
+        const material = new THREE.MeshStandardMaterial({ color: 0x101010 });
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.rotation.set(0.3, 0, 0);
+        scene.add(mesh);
+
+        const light = new THREE.PointLight(0xffffff, 500, 500);
+        light.position.set(0, 10, 0);
+        scene.add(light);
+
+        function updateSize() {
+            resizing = true;
+            const rect = canvasContainer.getBoundingClientRect();
+            lastKnownSize = { width: rect.width, height: rect.height };
+            if (bPhone) {
+                lastKnownSize.height += window.innerHeight * 4.5 / 100
+            } else {
+                lastKnownSize.width += window.innerHeight * 4.5 / 100
+            }
+        }
+
+        const resizeObserver = new ResizeObserver(() => {
+            if (resizing) {
+                requestAnimationFrame(render);
+            } else {
+                updateSize();
+            }
+        });
+
+        resizeObserver.observe(canvasContainer);
+
+        function render() {
+            mesh.rotation.y += 0.003;
+
+            if (resizing) {
+                camera.aspect = lastKnownSize.width / lastKnownSize.height;
+                camera.updateProjectionMatrix();
+                renderer.setSize(lastKnownSize.width, lastKnownSize.height);
+            }
+
+            renderer.render(scene, camera);
+
+            if (resizing) {
+                resizing = false;
+            }
+        }
+
+        function animate() {
+            requestAnimationFrame(animate);
+            render();
+        }
+        render();
+        animate();
+
+    } else {
+        const warning = WebGL.getWebGLErrorMessage();
+        document.getElementById('container').appendChild(warning);
+    }
+}
+
+init();
--- src/script.js
+++ src/script.js
@@ -4,39 +4,65 @@ const svgArrow1 = document.getElementById('svgArrow1');
 const svgArrow2 = document.getElementById('svgArrow2');
 const svgLine = document.getElementById('svgLine');
 var bMenu = false
+var bPhone = false
+if (window.innerWidth < 647) {
+    bPhone = true
+}
+
+if (!bPhone) {
+    svgArrow1.style.opacity = "0";
+}
 
 menuButton.addEventListener("mouseover", () => {
-    if (bMenu) {
-        svgArrow2.style.animation = "fadeIn 0.4s";
-        svgArrow2.style.opacity = 1;
-        svgArrow1.style.animation = "fadeOut 0.4s";
-        svgArrow1.style.opacity = 0;
-    } else {
-        svgArrow1.style.animation = "fadeIn 0.4s";
-        svgArrow1.style.opacity = 1;
-        svgArrow2.style.animation = "fadeOut 0.4s";
-        svgArrow2.style.opacity = 0;
+    if (!bPhone) {
+        if (bMenu) {
+            svgArrow2.style.animation = "fadeIn 0.4s";
+            svgArrow2.style.opacity = 1;
+            svgArrow1.style.animation = "fadeOut 0.4s";
+            svgArrow1.style.opacity = 0;
+        } else {
+            svgArrow1.style.animation = "fadeIn 0.4s";
+            svgArrow1.style.opacity = 1;
+            svgArrow2.style.animation = "fadeOut 0.4s";
+            svgArrow2.style.opacity = 0;
+        }
+        svgLine.style.animation = "fadeOut 0.4s";
+        svgLine.style.opacity = 0;
     }
-    svgLine.style.animation = "fadeOut 0.4s";
-    svgLine.style.opacity = 0;
 });
 
 menuButton.addEventListener("mouseout", () => {
-    svgLine.style.animation = "fadeIn 0.4s";
-    svgLine.style.opacity = 1;
-    svgArrow2.style.animation = "fadeOut 0.4s";
-    svgArrow2.style.opacity = 0;
-    svgArrow1.style.animation = "fadeOut 0.4s";
-    svgArrow1.style.opacity = 0;
+    if (!bPhone) {
+        svgLine.style.animation = "fadeIn 0.4s";
+        svgLine.style.opacity = 1;
+        svgArrow2.style.animation = "fadeOut 0.4s";
+        svgArrow2.style.opacity = 0;
+        svgArrow1.style.animation = "fadeOut 0.4s";
+        svgArrow1.style.opacity = 0;
+    }
 });
 
 menuButton.addEventListener("click", () => {
     bMenu = !bMenu
-    if (bMenu) {
-        document.getElementById("main").style.gridTemplateColumns = "20% 2% 78%";
-        document.getElementById("menuBox").style.transform = "translateX(0)"
+    if (!bPhone) {
+        if (bMenu) {
+            document.getElementById("main").style.gridTemplateColumns = "20% 2% 78%";
+            document.getElementById("menuBox").style.transform = "translateX(0)"
+        } else {
+            document.getElementById("main").style.gridTemplateColumns = "1% 2% 97%";
+            document.getElementById("menuBox").style.transform = "translateX(-20vw)"
+        }
     } else {
-        document.getElementById("main").style.gridTemplateColumns = "1% 2% 97%";
-        document.getElementById("menuBox").style.transform = "translateX(-20vw)"
+        if (bMenu) {
+            document.getElementById("main").style.gridTemplateRows = "35% 5% 60%";
+            svgArrow1.style.opacity = 1;
+            svgArrow2.style.opacity = 0;
+        } else {
+            document.getElementById("main").style.gridTemplateRows = "88% 5% 7%";
+            svgArrow2.style.opacity = 1;
+            svgArrow1.style.opacity = 0;
+        }
+
     }
+    console.log(bMenu)
 })
