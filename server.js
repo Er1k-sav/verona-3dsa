027ea1bd0e0f4b520fa55b68cfe94dba9a03cbf8
FA_pew <you@example.comfapew07@gmail.com>
2024-01-07 18:37:15 +0100

    ImagesCompressed

--- server.js
+++ server.js
@@ -1,51 +1,50 @@
-import express from "express";
-import path from 'path';
-import fs from 'fs';
-import { fileURLToPath } from 'url';
+import express from "express"
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-const app = express();
+const app = express()
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-    res.type("html");
+    res.sendFile(__dirname + "/index.html")
+    res.type("html")
 });
 
 app.get("/src/assets/style.css", (req, res) => {
-    res.sendFile(__dirname + "/src/assets/style.css");
-    res.type("css");
+    res.sendFile(__dirname + "/src/assets/style.css")
+    res.type("css")
 });
 
 app.get("/src/main.js", (req, res) => {
-    res.sendFile(__dirname + "/src/main.js");
-    res.type("application/javascript");
+    res.sendFile(__dirname + "/src/main.js")
+    res.type("application/javascript")
 });
 
 app.get("/src/script.js", (req, res) => {
-    res.sendFile(__dirname + "/src/script.js");
-    res.type("application/javascript");
+    res.sendFile(__dirname + "/src/script.js")
+    res.type("application/javascript")
 });
 
 app.get("/src/assets/data.js", (req, res) => {
-    res.sendFile(__dirname + "/src/assets/data.js");
-    res.type("application/javascript");
+    res.sendFile(__dirname + "/src/assets/data.js")
+    res.type("application/javascript")
 });
 
 app.get("/src/assets/images/map.png", (req, res) => {
-    res.sendFile(__dirname + "/src/assets/images/map.png");
-    res.type("image/png");
+    res.sendFile(__dirname + "/src/assets/images/map.png")
+    res.type("image/png")
 });
 
 app.get("/src/assets/images/logo.png", (req, res) => {
-    res.sendFile(__dirname + "/src/assets/images/logo.png");
-    res.type("image/png");
+    res.sendFile(__dirname + "/src/assets/images/logo.png")
+    res.type("image/png")
 });
 
 app.get("/src/assets/images/IISlogo.png", (req, res) => {
-    res.sendFile(__dirname + "/src/assets/images/IISlogo.png");
-    res.type("image/png");
+    res.sendFile(__dirname + "/src/assets/images/IISlogo.png")
+    res.type("image/png")
 });
 
 for (let i = 0; i < 19; i++) {
@@ -60,9 +59,9 @@ let nFiles = [0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 for (let i = 0; i < 19; i++) {
     for (let j = 0; j < nFiles[i]; j++) {
         app.get(`/src/assets/images/infos/${i}/${j}.png`, (req, res) => {
-            res.sendFile(__dirname + `/src/assets/images/infos/${i}/${j}.png`);
-            res.type("image/png");
-        });
+            res.sendFile(__dirname + `/src/assets/images/infos/${i}/${j}.png`)
+            res.type("image/png")
+        })
     }
 }
 
