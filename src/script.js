06be6ae0e03d71c3d201ebb1b147c9b78ed8c9ab
FA_pew <you@example.comfapew07@gmail.com>
2024-01-15 19:52:27 +0100

    AddedGoogleMapIntegration

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 console.log("script.js")
 
-import { names, bInfos } from "../src/assets/data.js"
+import { names, bInfos, mapIDs, iAdr, iOpnH } from "../src/assets/data.js"
 
 const menuButton = document.getElementById("mSvg")
 const svgArrow1 = document.getElementById('svgArrow1');
@@ -12,7 +12,7 @@ var bPhone = false
 var bInfo = false
 var bSrc = false
 infoOff()
-//objClick(3)
+objClick(3)
 if (window.innerWidth < 769) {
     bPhone = true
 }
@@ -97,18 +97,21 @@ export function objClick(idx) {
     console.log(idx)
     document.getElementById("info").style.visibility = "visible";
     document.getElementById("iTitle").innerHTML = names[idx].toUpperCase()
+    iGmap(idx)
     document.getElementById("iBody").innerHTML = document.getElementById("iGmaps").outerHTML + bInfos[idx].outerHTML
     document.getElementById("shadow").style.zIndex = 2
-    iGmap(idx)
+    
     if (bMenu) {
         menu()
     }
 }
 
+
+
 function iGmap(idx) {
-    document.getElementById("iAdr").innerHTML//* = <ADDRESS>
-    document.getElementById("iOpnH").innerHTML//* = <ORARI>
-    document.getElementById("iGmapsBox").innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d${idx.cords[0]}!2d10.987825374773774!3d${idx.cords[1]}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477f5f68699be0e3%3A0x53f85a636882595b!2sVerona%20VR!5e0!3m2!1sit!2sit!4v1705332243896!5m2!1sit!2sit" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+    document.getElementById("iAdr").innerHTML = iAdr[idx]
+    document.getElementById("iOpnH").innerHTML = iOpnH[idx]
+    document.getElementById("iGmapsBox").innerHTML = `<iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=100%25&amp;hl=en&amp;q=${mapIDs[idx].replaceAll(" ", "%20")}+(${mapIDs[idx].replaceAll(" ", "%20")})&amp;t=p&amp;z=16&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/population/">`
 }
 
 document.getElementById("results").addEventListener("click", function(event) {
