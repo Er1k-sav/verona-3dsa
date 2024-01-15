8357bbf4a6bbb5abd662251d35720b786e91bff4
Er1k-sav <esavpost@gmail.com>
2024-01-15 16:27:31 +0100

    addedGooglemapsembed

--- src/script.js
+++ src/script.js
@@ -108,7 +108,7 @@ export function objClick(idx) {
 function iGmap(idx) {
     document.getElementById("iAdr").innerHTML//* = <ADDRESS>
     document.getElementById("iOpnH").innerHTML//* = <ORARI>
-    document.getElementById("iGmapsBox").innerHTML//* = <MAPPA>
+    document.getElementById("iGmapsBox").innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d${idx.cords[0]}!2d10.987825374773774!3d${idx.cords[1]}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477f5f68699be0e3%3A0x53f85a636882595b!2sVerona%20VR!5e0!3m2!1sit!2sit!4v1705332243896!5m2!1sit!2sit" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
 }
 
 document.getElementById("results").addEventListener("click", function(event) {
